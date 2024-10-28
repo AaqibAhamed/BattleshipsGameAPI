426560ecf570ebe198f0d2bc4ecf7d6a3f294d8a
Aaqib Wiki <aaqibgahamed@gmail.com>
2024-10-28 21:29:27 +0530

    Add project files.

---
+++ BattleshipsGameAPI/Controllers/BattleshipGameController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BattleshipsGameAPI.Controllers
+{
+    public class BattleshipGameController : ControllerBase
+    {
+        private static Game game;
+
+        [HttpPost("start")]
+        public IActionResult StartGame()
+        {
+            game = new Game();
+            game.PlaceShips();
+            return Ok("Game started!");
+        }
+
+        [HttpGet("status")]
+        public IActionResult GetStatus()
+        {
+            return Ok(game.GetBoardStatus());
+        }
+
+        [HttpPost("shot")]
+        public IActionResult Shoot([FromBody] string shot)
+        {
+            var result = game.ProcessShot(shot);
+            if (!result.IsValid)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok(result.Message);
+        }
+
+      
+    }
+}
